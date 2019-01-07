d05ca4eeb7aeafb77d63df9332926a635d81e794
oneapp <oneappmarketing@gmail.com>
2019-01-07 11:30:29 +0000

    owl added

---
+++ owl/owl.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('#top-songs').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+
+    $('.owl-prev').html('<i class="fa fa-chevron-left></i>"')
+    $('.owl-next').html('<i class="fa fa-chevron-right></i>"')
+  });
