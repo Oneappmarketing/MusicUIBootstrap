2f13d7a0be466dce0ec845e7a22ff14a05a26494
oneapp <oneappmarketing@gmail.com>
2019-01-07 12:12:34 +0000

    top songs finished

--- owl/owl.js
+++ owl/owl.js
@@ -16,6 +16,24 @@ $(document).ready(function(){
         }
     })
 
+
+    $('#top-albums').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
     $('.owl-prev').html('<i class="fa fa-chevron-left></i>"')
     $('.owl-next').html('<i class="fa fa-chevron-right></i>"')
   });
